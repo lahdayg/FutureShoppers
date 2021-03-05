f145493d20604a323283abacf25685f095849162
Akin Oladeji <lahdayg@yahoo.com>
2021-03-05 15:34:10 +0000

    This is my first commit

---
+++ FutureShoppers/Utilities/FileLocation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace FutureShoppers.Utilities
+{
+    class FileLocation
+    {
+
+        public string GetFolderLocation(string folder)
+        {
+            return Directory.GetParent(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location).ToString(), @"..\..\")) + @"\" + folder + @"\";
+
+        }
+    }
+}
+
+
+    
+
