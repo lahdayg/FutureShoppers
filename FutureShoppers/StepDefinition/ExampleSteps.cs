57741756afefa74beb37a24028432ff3d469d05c
Akin Oladeji <lahdayg@yahoo.com>
2021-03-05 15:48:02 +0000

    Folders created

---
+++ FutureShoppers/StepDefinition/ExampleSteps.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace FutureShoppers.StepDefinition
+{
+    [Binding]
+    public class ExampleSteps
+    {
+        [Given(@"the first number is (.*)")]
+        public void GivenTheFirstNumberIs(int p0)
+        {
+            ScenarioContext.Current.Pending();
+
+        }
+        
+        [Given(@"the second number is (.*)")]
+        public void GivenTheSecondNumberIs(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"the two numbers are added")]
+        public void WhenTheTwoNumbersAreAdded()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the result should be (.*)")]
+        public void ThenTheResultShouldBe(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
